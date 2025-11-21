ca9b0100d089071fae93d8dbcf6c57a2f067d8d5
tania-macheieva <macheieva.tetiana@gmail.com>
2025-11-21 19:38:45 +0200

    Resolve Uncaught TypeError when loading tech icons

--- js/techIcons.js
+++ js/techIcons.js
@@ -1,20 +1,27 @@
-const techIconsContainer = document.querySelector(".tech-icons-container");
-const icons = [
-	"devicon-csharp-plain",
-	"devicon-ruby-plain",
-	"fab fa-docker",
-	"devicon-dotnetcore-plain",
-	"devicon-rails-plain",
-	"fab fa-github",
-	"fa-solid fa-database",
-	"fab fa-react",
-	"fab fa-node-js",
-	"fab fa-git-alt",
-];
+document.addEventListener('DOMContentLoaded', () => {
+    const techIconsContainer = document.querySelector(".tech-icons-container"); 
+    
+    if (techIconsContainer) {
+        const icons = [
+            "devicon-csharp-plain",
+            "devicon-ruby-plain",
+            "fab fa-docker",
+            "devicon-dotnetcore-plain",
+            "devicon-rails-plain",
+            "fab fa-github",
+            "fa-solid fa-database",
+            "fab fa-react",
+            "fab fa-node-js",
+            "fab fa-git-alt",
+        ];
 
-icons.forEach((iconClass, index) => {
-	const div = document.createElement("div");
-	div.className = "tech-icon";
-	div.innerHTML = `<i class="${iconClass}"></i>`;
-	techIconsContainer.appendChild(div);
+        icons.forEach((iconClass) => {
+            const div = document.createElement("div");
+            div.className = "tech-icon";
+            const i = document.createElement("i");
+            i.className = iconClass;
+            div.appendChild(i);
+            techIconsContainer.appendChild(div);
+        });
+    }
 });
